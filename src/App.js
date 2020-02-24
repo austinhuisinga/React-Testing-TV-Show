6e6bb733f901771dfc54ed3f305bc2c354d44180
austinhuisinga <57517058+austinhuisinga@users.noreply.github.com>
2020-02-24 14:18:31 -0700

    fixed index.html

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ export default function App() {
           setShow(res.data);
           setSeasons(formatSeasons(res.data._embedded.episodes));
       })
-    fetchShow();
+    // fetchShow();
   }, []);
 
   // useEffect(() => {
